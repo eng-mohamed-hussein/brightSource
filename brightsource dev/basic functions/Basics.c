9ac12c4191d96c5a4bdd0b9bf9fa96386e940568
mohamed hussein <m.hussein1389@gmail.com>
2016-10-20 10:50:26 +0200

    adding delay function in basic functions

---
+++ brightsource dev/basic functions/Basics.c
@@ -0,0 +1,37 @@
+
+/*************** basic includes and definitions **************/
+
+#include"Basics.h"
+
+/******************* functions definition *******************/
+
+/* Initializing timer0*/
+
+void initDelayTimer(void)
+{
+	 /* enable timer0  */
+		setBit(SYSCTL -> RCGCTIMER,BIT0); 
+	 /* clear timer zero control registser  pin TnEN*/
+		clearBit(TIMER0 -> CTL,BIT0); 
+	 /* cleared to concatenate A,B timer registers */	
+		clearAllBits(TIMER0 -> CFG);
+	 /* set timer to one-shot mode	 */	 
+		setBit(TIMER0 -> TAMR,BIT0);	
+	 /* clear TACDIR as count down timer	*/	
+		clearBit(TIMER0 -> TAMR,BIT4); 		
+	
+}
+ 
+void delay_ms(uint32_t time)
+{
+	 /* setting timer equation to be meassured in MS*/
+	  time = time * (SystemCoreClock/1000);
+	 /* configure the TIMER0 with the value i want it to count */ 
+		TIMER0->TAILR = time;		
+		setBit(TIMER0 -> CTL,BIT0);
+	 /* checking if TIMER0 finished */
+		while(checkBit(TIMER0 -> RIS,BIT0) != 1);
+	 /* clear timer finish flag to be able to count back again and that by forcing one */
+		setBit(TIMER0 -> ICR,BIT0);
+}
+ 
