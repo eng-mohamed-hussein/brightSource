806efd31c8a1365dfc2b242b5ffd633ba0f2fd58
mohamed hussein <m.hussein1389@gmail.com>
2016-10-23 16:20:24 +0200

    update adc driver

--- brightsource dev/adc/ADC1.c
+++ brightsource dev/adc/ADC1.c
@@ -1,31 +1,25 @@
 # include <ADC1.h>
 
-void init_ADC1(void)
+void initADC1(void)
 {
-	SYSCTL->RCGCADC = (1<<1);        /*use ADC1 2  16 MHZ*/
-	SYSCTL->RCGCGPIO = (1<<4);
-	GPIOE->DIR &= ~(1<<1);
-	GPIOE->AFSEL = (1<<1);          //pE1
-	GPIOE->DEN  &=~(1<<1);  //disable  digital
+	SYSCTL->RCGCADC = (1<<1);       	/*use ADC1 2  16 MHZ*/
+	SYSCTL->RCGCGPIO = (1<<4);       	/*set PORTE as GPIO */
+	GPIOE->DIR &= ~(1<<1);				/*use pin 1 as I/P  */
+	GPIOE->AFSEL = (1<<1);              /*pE1 */
+	GPIOE->DEN  &=~(1<<1);              /*disable  digital */
+	GPIOE->AMSEL = (1<<1);              /* diable circuts with this  pin */
 	
-	GPIOE->AMSEL = (1<<1);   //diable circuts with this  pin
-	/*sample sequancer SS3 as it  one bit i/p */
+	/*sample sequencer SS3 as it  one bit i/p */
 	
-	ADC1->ACTSS &= ~(1<<3);     //disblae sequncer
-	
-	ADC1->EMUX =(0xf<<12);   //tirger will start as sson as the adc enable  "continous sample "
-	
-	
-	ADC1->SSMUX3 = 2;   //analgo input 
-	
-	ADC1->SSCTL3 = 0x6;  //enable intterupt 
-	
-	
-	ADC1->ACTSS |= (1<<3); // enable the sequancer
+	ADC1->ACTSS &= ~(1<<3);     		/*disblae sequncer*/
+	ADC1->EMUX =(0xf<<12);   			/*tirger will start as soon as the adc enable  "continous sample " */
+	ADC1->SSMUX3 = 2;   				/*analog input */
+	ADC1->SSCTL3 = 0x6;  				/*enable intterupt */
+	ADC1->ACTSS |= (1<<3); 				/* enable the sequencer */
 }
 
 
-uint32_t Get_ADC(void)
+uint32_t readADC(void)
 {
 	volatile static uint32_t adcResult =0;
 	adcResult = ADC1->SSFIFO3;
--- brightsource dev/adc/ADC1.h
+++ brightsource dev/adc/ADC1.h
@@ -2,9 +2,11 @@
 /* This driver using  ADC1 module and  with sample sequencer SS3,*/
 /* trigger event  is "always sample" and no.of pin inputs is one  bit PE1*/
 
-void Initial_ADC1(void);
+void initADC1(void);
 
 /*getting  sample will be without any interrupt  so it polling function*/
 
+/*gets the converted I/P */
+/*unsigned int32 returned value */
 
-uint32_t Get_ADC(void);
+uint32_t readADC1(void); 
